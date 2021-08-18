73c147c521d65e5617bcb755c5575f72592e7ae3
ManniG24 <egatica51@gmail.com>
2021-08-18 13:37:11 -0600

    finished project

--- index.js
+++ index.js
@@ -39,12 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
 
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10) {
+    this.stomach.push(edible);
+  }
+}
 
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
 
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
 
 
 
@@ -63,10 +76,17 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+// Constructor for Car
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
   
 }
-
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
 
 /*
   TASK 3
@@ -75,18 +95,24 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+// inhert methods from person 
+Baby.prototype = Object.create(Person.prototype);
 
-
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`;
+}
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/Global Object Binding - If this isn't any of the other three it returns the window object. 
+  2. Implicit Binding - this refers to the object being passed, aka whats left of the dot.
+  3. New binding - when working with contructors you use the key word NEW to create a new object. the new object is referenced as this
+  4. Explicit binding
+  when using .call or .apply
 */
 
 
